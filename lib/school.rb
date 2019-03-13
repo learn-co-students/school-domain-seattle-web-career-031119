632ba5a2f17c793158a73d432fc6156ca2485b4b
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-03-12 17:39:12 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+class School
+  attr_reader :name, :roster
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade].nil? ? @roster[grade] = [name] : @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = @roster.reduce({}) {|sorted_roster, (k, v)|
+      sorted_roster[k] = v.sort
+      binding.pry
+      sorted_roster
+    }
+
+  end
+
+end
