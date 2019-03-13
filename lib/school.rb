25de6596636a2efda22e67de3e3f2e425cf83cef
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-03-13 13:51:44 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+
+
+class School
+  def initialize(name)
+    @school_name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if !@roster.has_key?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_hash = {}
+    @roster.each {|key, value|
+      new_hash[key] = value.sort
+    }
+    new_hash
+  end
+end
